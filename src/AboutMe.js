21a263dde1343fa5c15d90aedd5240ae0cfa1613
Amari James <73178321+maddevred@users.noreply.github.com>
2023-06-08 16:37:31 -0400

    Initial commit

---
+++ src/AboutMe.js
@@ -0,0 +1,33 @@
+const aboutMe =
+<html>
+<body>
+
+<br></br>
+
+<img class="img1" src="https://lh3.googleusercontent.com/fife/AAWUweVweYsLEHcst1H7OIXhvDFC8ZtlLtga5_qfBFEB9rgp-B0K0oZSEqr668Q3BRBgTXO3HYmimxLg549PD_MaW9LAU7fb736lSKgyhlhYR7JNRoEm_nirAv63s8ldYR5XqVkUk7mgkCdC4fLgpEEZOaogWc8V7P0v1tDfFkOcDa8lotNtltPAc2WfdlDUVdtX_RLfzmBwEYVDNcUnF9C7cX7A2eNyD3IZCwUrP36uGID9PHT3B3Ao-VWKpZ0KrWhQWjQP237eyxcfeNFv06BlEgNl6oGAMTyPdvRLdJ8A17fMUgn2ZFZ8TMoc_eZScDrkVK2gQi6Ly7Dr7UgbtU2pvKiYwTKcIXNSCUJH5UATk3qAPlrIJGMHWnQ6R8YXL3EKcPUtgWTopRMkeIDErt2XyUWWkts-3vOvSZYuG9qvfg_d2CfjMDYM4KVRcCPlEgSCAwRTEjeHCoPtg-M87m3V0cn1OfxVXn-wf7ba37ZKF1oTZjKBobGoAMv17_RPk_XwOgn-fDz0ov6SV11O9AtLaw58M9Fp6jtp9GqNSMs1s88YhRe6oRoPNvIuPTAdEXVDMdh9UbcWJFgUhdZyG2FthlbM9xwIg4mqf4PCpHmN89lNMIBalYajVDogFlar2xUfpCb4x9i1u6v4CGFSmU_DPTVKQu-guYbZi44HCBC9kVMlbWymqrC4YUm_tJExKVYcacMorEk6bGz-_WYbTKCyckk=w155-h194-no?authuser=0" alt="A current headshot."></img>
+
+<div class="dark fire">
+        <h1 class="Blazing" contenteditable="true">Amari James</h1> 
+</div>  
+
+
+{/* want to have subtitle blazing too  */}
+{/* <div class="dark fire">
+        <h4 class="Subtitles" contenteditable="true">Full-Stack Software Developer</h4>
+</div>   */}
+
+
+<strong><p>Full-Stack Software Developer</p></strong>
+<p> Specializing in adaptive, polished, and prompt professional software development.</p>
+<p><em>Click <a target="_blank" href="https://docs.google.com/document/d/e/2PACX-1vQ59KBLaNe3jrgolLLHSFgYDBlF3qQH01B2AWKgle-5J_KpaoykEnczfyg-IA9cXno464yAWcVYQPLf/pub"><strong>here</strong></a> to view resume!</em></p>
+
+<script>
+
+document.getElementById("demo").innerHTML = 5 + 6;
+
+</script>
+
+</body>
+</html>
+
+export default aboutMe;
